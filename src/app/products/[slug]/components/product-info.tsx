92d70dd361aa3dd27d1339b4b4443937159b90f5
adadezer <adadezer@gmail.com>
2023-12-13 15:23:58 -0300

    feat: ensure product can be added to cart

--- src/app/products/[slug]/components/product-info.tsx
+++ src/app/products/[slug]/components/product-info.tsx
@@ -3,14 +3,12 @@
 import { Button } from "@/components/ui/button";
 import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
+import { CartContext } from "@/providers/cart";
 import { ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 interface ProductInfoProps {
-  product: Pick<
-    ProductWithTotalPrice,
-    "basePrice" | "description" | "discountPercentage" | "totalPrice" | "name"
-  >;
+  product: ProductWithTotalPrice;
 }
 
 const formatNumber = new Intl.NumberFormat("pt-BR", {
@@ -18,10 +16,10 @@ const formatNumber = new Intl.NumberFormat("pt-BR", {
   currency: "BRL",
 });
 
-const ProductInfo = ({
-  product: { basePrice, description, discountPercentage, totalPrice, name },
-}: ProductInfoProps) => {
+const ProductInfo = ({ product }: ProductInfoProps) => {
   const [quantity, setQuantity] = useState(1);
+
+  const { addProductToCart } = useContext(CartContext);
 
   const handleDecreaseQuantityClick = () => {
     setQuantity((qtd) => (qtd === 1 ? qtd : qtd - 1));
@@ -31,21 +29,25 @@ const ProductInfo = ({
     setQuantity((qtd) => qtd + 1);
   };
 
+  const handleAddCartToClick = () => {
+    addProductToCart({ ...product, quantity });
+  };
+
   return (
     <div className="flex flex-col px-5">
-      <h2 className="text-lg">{name}</h2>
+      <h2 className="text-lg">{product.name}</h2>
 
       <div className="flex items-center gap-2">
-        <h1 className="text-xl font-bold">{totalPrice}</h1>
+        <h1 className="text-xl font-bold">{product.totalPrice}</h1>
 
-        {discountPercentage > 0 && (
-          <DiscountBadge>{discountPercentage}</DiscountBadge>
+        {product.discountPercentage > 0 && (
+          <DiscountBadge>{product.discountPercentage}</DiscountBadge>
         )}
       </div>
 
-      {discountPercentage > 0 && (
+      {product.discountPercentage > 0 && (
         <p className="text-sm line-through opacity-40">
-          De: {String(formatNumber.format(Number(basePrice)))}
+          De: {String(formatNumber.format(Number(product.basePrice)))}
         </p>
       )}
 
@@ -71,10 +73,15 @@ const ProductInfo = ({
 
       <div className="mt-8 flex flex-col gap-3">
         <h3 className="font-bold">Descrição</h3>
-        <p className="text-justify text-sm opacity-60">{description}</p>
+        <p className="text-justify text-sm opacity-60">{product.description}</p>
       </div>
 
-      <Button className="mt-8 font-bold uppercase">Adicionar Carrinho</Button>
+      <Button
+        className="mt-8 font-bold uppercase"
+        onClick={handleAddCartToClick}
+      >
+        Adicionar Carrinho
+      </Button>
 
       <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
         <div className="flex items-center gap-2">
