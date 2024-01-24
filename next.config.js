f3b502eaf50698a587bde50661f1b51ad2df8536
adadezer <adadezer@gmail.com>
2024-01-24 17:28:38 -0300

    fix: fix dropbox domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fsw-store.s3.sa-east-1.amazonaws.com", "i.imgur.com", "www.dropbox.com"]
+    domains: ["fsw-store.s3.sa-east-1.amazonaws.com", "i.imgur.com", "www.dropbox.com", "dl.dropboxusercontent.com"]
   },
   experimental: {
     serverActions: true,
